276de3b71a590a960894d0aa7d42164c53492565
Marmik Pandya <marmik.pandya@capgemini.com>
2016-09-22 03:21:41 +0000

    basic stuff got workoing

--- miditest.js
+++ miditest.js
@@ -1,5 +1,4 @@
 var colors = require('colors');
-var sampleMidi = require('./MidiSampler');
 var fs = require('fs');
 var Midi = require('jsmidgen');
 var dnn = require('dnn');
@@ -7,12 +6,83 @@ var dnn = require('dnn');
 var NOTE_OFFSET = 45;
 var WINDOW_WIDTH = 32;
 
-var files = [];
-for(var i = 1; i <= 53; i++) {
-  files.push('/ledZep/'+i+'.mid');
+var files = ['0.mid'];
+/*for(var i = 0; i <= 53; i++) {
+  files.push(+i+'.mid');
+}
+*/
+console.log(files);
+
+//files = ['zepplicained.mid']
+
+
+
+
+
+
+
+function sampleMidi(path, offset) {
+  console.log("inside sample midi");
+  var midiFileParser = require('midi-file-parser');
+  var file = require('fs').readFileSync(path, 'binary');
+  var midi = midiFileParser(file);
+  console.log(midi.header.trackCount);
+
+  var trackResolution = midi.header.ticksPerBeat;
+  var ticksPerSixteenth = trackResolution / 8;
+  var trackNumber = (midi.header.trackCount == 1) ? 0 : 1;
+
+  var notes = {};
+  var noteCount = 0;
+
+  for(i in midi.tracks[trackNumber]) {
+    var event = midi.tracks[trackNumber][i];
+    if (event.subtype == 'noteOn' && !notes[event.noteNumber]) {
+      notes[event.noteNumber] = true;
+      noteCount++;
+    }
+  }
+
+  for(i in notes) {
+    notes[i] = false;
+  }
+
+  var samples = [];
+  var currentTick = 0;
+  var sampleIndex = 0;
+
+  for(i in midi.tracks[trackNumber]) {
+    var event = midi.tracks[trackNumber][i];
+    currentTick += event.deltaTime;
+    if (ticksPerSixteenth * sampleIndex < currentTick) {
+      var sample = generateEmptySample();
+      for(i in notes) {
+        if(i - offset < 40 && i - offset >= 0)
+          sample[i - offset] = notes[i] ? 1 : 0;
+      }
+      samples.push(sample);
+      sampleIndex++;
+    }
+    if (event.subtype == 'noteOn' && event.type == 'channel') {
+      notes[event.noteNumber] = true;
+    } else if (event.subtype == 'noteOff' && event.type == 'channel') {
+      notes[event.noteNumber] = false;
+    }
+  }
+
+  return samples;
 }
 
-files = ['Prelude17.mid']
+function generateEmptySample() {
+  var sample = [];
+  for(var i = 0; i < 40; i++)
+    sample.push(0);
+  return sample;
+}
+
+
+
+
 
 /*
  *  Extract samples from files
@@ -20,9 +90,10 @@ files = ['Prelude17.mid']
 
 var data = [];
 for(var i = 0; i < files.length; i++) {
-  if(!fs.existsSync(files[i]))
-    continue;
+  console.log("inside files");
+
   var samples = sampleMidi(files[i], NOTE_OFFSET);
+  console.log("Samples");
   for(var j = 0; (j + 1) * WINDOW_WIDTH < samples.length; j++) {
     var submatrix = extractColumns(samples, j * WINDOW_WIDTH, WINDOW_WIDTH);
     data.push(unfold(submatrix));
@@ -30,7 +101,6 @@ for(var i = 0; i < files.length; i++) {
   console.log("Parsed " + i + "th file");
 }
 console.log(data.length);
-
 
 
 function extractColumns(arr, start, length) {
@@ -77,20 +147,20 @@ function printMatrix(m) {
  *  Create model
  */
 
-function createRBN(input, output, data) {
+function createRBN(visible, output, data) {
   var rbm = new dnn.RBM({
       input : data,
-      n_visible : input,
+      n_visible : visible,
       n_hidden : output
   });
 
-  rbm.set('log level', 2);
+  rbm.set('log level', 0);
 
-  var trainingEpochs = 150;
+  var trainingEpochs = 500;
 
   rbm.train({
       lr : 0.6,
-      k : 3,
+      k : 1,
       epochs : trainingEpochs
   });
 
